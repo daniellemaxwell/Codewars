e88a3f4dfb3ce9678cad9e33a2a8c47e12de0737
daniellemaxwell <daniellemmaxwell@gmail.com>
2022-05-11 13:08:11 -0400

    Function summing positive nums in an array

---
+++ Sum of Positives/main.js
@@ -0,0 +1,7 @@
+/* You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0. */
+
+const sumPositives = nums => nums.filter(num => num > 0).reduce((num, x) => num + x, 0)

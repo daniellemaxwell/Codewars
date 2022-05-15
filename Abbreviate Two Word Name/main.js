146e1ad0818cb43daac5518aa40b20f926636085
daniellemaxwell <daniellemmaxwell@gmail.com>
2022-05-15 17:23:13 -0400

    Corrected spacing error in lastInitial variable

--- Abbreviate Two Word Name/main.js
+++ Abbreviate Two Word Name/main.js
@@ -11,6 +11,6 @@ patrick feeney => P.F */
 function abbrevName(name) {
     let array = name.split(' ')
     let firstInitial = array[0].slice(0,1)
-    let secondInitial = array [1].slice(0,1)
+    let secondInitial = array[1].slice(0,1)
     return (firstInitial + '.' + secondInitial).toUpperCase()
 }

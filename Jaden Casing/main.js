7b65adb513db2ab98741d28007b9849968824be8
daniellemaxwell <daniellemmaxwell@gmail.com>
2022-05-22 09:55:44 -0400

    Made slice function cleaner

--- Jaden Casing/main.js
+++ Jaden Casing/main.js
@@ -9,6 +9,6 @@ Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real" */
 
 function jadenCase(string) {
     return string.split(' ')
-           .map(word => word[0].toUpperCase() + word.slice(1, word.length))
+           .map(word => word[0].toUpperCase() + word.slice(1))
            .join(' ')
 }

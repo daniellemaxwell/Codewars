51f742ec8876f6ca68e7809f563963815191a983
daniellemaxwell <daniellemmaxwell@gmail.com>
2022-06-21 15:47:34 -0400

    Function to find the shortest word in a string

---
+++ Shortest Word/main.js
@@ -0,0 +1,9 @@
+/* DESCRIPTION:
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types. */
+
+function shortestWord(string) {
+    let length = string.split(' ').map(el => el.length)
+    return Math.min(...length)
+}
